39dbadc48ad7bbe0ee42ee241f8cf3b17adae1f0
cspencer <cspencernd@gmail.com>
2023-09-22 11:33:11 -0400

    perf: rm the hydrated check; this method better;

--- src/components/theme/theme-provider.tsx
+++ src/components/theme/theme-provider.tsx
@@ -3,12 +3,7 @@
 import * as React from "react"
 import { ThemeProvider as NextThemeProvider } from "next-themes"
 import { ThemeProviderProps } from "next-themes/dist/types"
-import { useHydrated } from "@/lib/hooks"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-    const hydrated = useHydrated()
-
-    if (!hydrated) return null
-
     return <NextThemeProvider {...props}>{children}</NextThemeProvider>
 }
